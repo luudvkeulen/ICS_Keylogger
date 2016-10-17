d235f7ece79b2786fed16f78558ef287985af3ff
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-10-17 17:51:32 +0200

    Removed unused stuff

--- CustomKeyboard/CustomKeyboard.ino
+++ CustomKeyboard/CustomKeyboard.ino
@@ -7,75 +7,40 @@
 
 class KbdRptParser : public KeyboardReportParser
 {
-    void PrintKey(uint8_t mod, uint8_t key);
-
-  protected:
-    virtual void OnKeyDown	(uint8_t mod, uint8_t key);
-    virtual void OnKeyPressed(uint8_t key);
-};
-
-void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
-{
-  MODIFIERKEYS mod;
-  *((uint8_t*)&mod) = m;
-  Serial.print((mod.bmLeftCtrl   == 1) ? "C" : " ");
-  Serial.print((mod.bmLeftShift  == 1) ? "S" : " ");
-  Serial.print((mod.bmLeftAlt    == 1) ? "A" : " ");
-  Serial.print((mod.bmLeftGUI    == 1) ? "G" : " ");
-
-  Serial.print(" >");
-  PrintHex<uint8_t>(key, 0x80);
-  Serial.print("< ");
-
-  Serial.print((mod.bmRightCtrl   == 1) ? "C" : " ");
-  Serial.print((mod.bmRightShift  == 1) ? "S" : " ");
-  Serial.print((mod.bmRightAlt    == 1) ? "A" : " ");
-  Serial.println((mod.bmRightGUI    == 1) ? "G" : " ");
+  protected:virtual void OnKeyDown	(uint8_t mod, uint8_t key);
 };
 
 void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
 {
+  if (key == 40) {
+    Serial.println("");
+  } else if (key <= 127) {
+    uint8_t ascii = OemToAscii(mod, key);
+    Serial.print((char)ascii);
+  }
   //Serial.print("DN ");
   //PrintKey(mod, key);
-  uint8_t c = OemToAscii(mod, key);
+  //uint8_t c = OemToAscii(mod, key);
 
-  if (c)
-    OnKeyPressed(c);
+  //if (c)
+  //OnKeyPressed(c);
 }
 
-void KbdRptParser::OnKeyPressed(uint8_t key)
-{
-  if(key == 176) {
-    Serial.println("Enter pressed");
-  }
-  
-  Serial.print("ASCII: ");
-  Serial.println((char)key);
-};
-
 USB     Usb;
+//If the keyboard has a hub (Apple keyboard) then combine them
 USBHub     Hub1(&Usb);
-//USBHub     Hub2(&Usb);
-//USBHub     Hub3(&Usb);
-//USBHub     Hub4(&Usb);
-//USBHub     Hub5(&Usb);
-//USBHub     Hub6(&Usb);
 HIDBoot<HID_PROTOCOL_KEYBOARD>    HidKeyboard1(&Usb);
-//HIDBoot<HID_PROTOCOL_KEYBOARD>    HidKeyboard2(&Usb);
-//HIDBoot<HID_PROTOCOL_KEYBOARD>    HidKeyboard3(&Usb);
 
 KbdRptParser Prs;
 
 void setup()
 {
   Serial.begin( 115200 );
-  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+  while (!Serial);
   Serial.println("Starting...");
 
   if (Usb.Init() == -1)
     Serial.println("OSC did not start.");
-
-  //delay(200);
 
   HidKeyboard1.SetReportParser(0, (HIDReportParser*)&Prs);
 }
